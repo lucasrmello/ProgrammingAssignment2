4ac5c547181cf0ace3c54d0946da7fd629b0a36a
lucasrmello <lucasr.mello@hotmail.com>
2015-09-27 08:23:59 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hello, here is my code, the first function will store a matrix in te global enviroment and the second one is supposed to
+## get you the inverse of this matrix using the solve() function
+## in the end of the script there is already an example of check for the sake of saving you some time when evaluating the code
 
 ## Write a short comment describing this function
 
@@ -25,3 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
     x$setinverse(inv)
     inv
   }
+  
+ ## example 
+  x = rbind(c(1, -1/4), c(-1/4, 1))
+  example = makeCacheMatrix(x)
+  example$get()
+  
+  cacheSolve(example)
